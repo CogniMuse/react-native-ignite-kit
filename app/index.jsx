fca7f3fdac2def72a81fffc47f1ad5a30e272f1a
Sandeshbhat234 <101869011+Sandeshbhat234@users.noreply.github.com>
2025-04-30 18:27:45 +0530

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View className="flex items-center justify-center h-screen bg-black">
+      <Text className="text-4xl text-[#CDF683]">Welcome bruh 😎</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
